74d173f72016753cbf57d812e8017d2b97754461
yomites <marshall_400@yahoo.com>
2020-06-08 23:34:27 +0300

    Separate modules
    Exercise 4.2: Blog list step 2. Refractoring the application into separate modules.

--- blogposts/index.js
+++ blogposts/index.js
@@ -1,59 +1,10 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const url = process.env.MONGODB_URI
+const server = http.createServer(app)
 
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(result => {
-    console.log('connected to MongoDB')
-  }).catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-  morgan.token('content', function (req, res) {
-    console.log('------------------------------------------------')
-    res.end()
-    return JSON.stringify(req.body)
-  })
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
-app.use(cors())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
