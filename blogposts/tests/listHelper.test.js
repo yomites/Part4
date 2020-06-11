52468229b141b491b54bb110ec390518223f5f35
yomites <marshall_400@yahoo.com>
2020-06-11 21:05:27 +0300

    mostBlogs function
    Exercise 4.6: Helper functions and unit tests step 7

--- blogposts/tests/listHelper.test.js
+++ blogposts/tests/listHelper.test.js
@@ -253,14 +253,138 @@ describe('Blog with most likes', () => {
 
     test('in a blog list with many blogs is the one with highest likes', () => {
         const result = listHelper.favoriteBlog(listWithManyBlogs)
-        console.log({
-            title: listWithManyBlogs[4].title, 
-            author: listWithManyBlogs[4].author, 
-            likes: listWithManyBlogs[4].likes})
         expect(result).toEqual({
             title: listWithManyBlogs[4].title, 
             author: listWithManyBlogs[4].author, 
             likes: listWithManyBlogs[4].likes
+        })
+    })
+})
+
+describe('Author with most blogs', () => {
+    const listWithEmptyContent = []
+
+    const listWithOneBlog = [
+        {
+            title: "HTML is easy",
+            author: "Ben Morrisson",
+            url: "http:mybusiness.com",
+            likes: 5,
+            id: "5edddee2582b5d2174735d04"
+        }
+    ]
+    const listWithManyBlogs = [
+        {
+            title: "HTML is easy",
+            author: "Ben Morrisson",
+            url: "http:mybusiness.com",
+            likes: 5,
+            id: "5edddee2582b5d2174735d04"
+        },
+        {
+            title: "Take me home tonight",
+            author: "Sam Blake",
+            url: "www.kanayo.com",
+            likes: 4,
+            id: "5edde693d25a7946a41425d1"
+        },
+        {
+            title: "Postman is cool too",
+            author: "Brad Sparrow",
+            url: "www.postman.com",
+            likes: 12,
+            id: "5edde96fd2278a3a8c121dc8"
+        },
+        {
+            title: "Take me home tonight and forever",
+            author: "Sammy Blaker",
+            url: "www.kanayo.com",
+            likes: 10,
+            id: "5edde716d25a7946a41425d2"
+        },
+        {
+            title: "Power of Javascript",
+            author: "Marja Alonen",
+            url: "www.javascript.com",
+            likes: 25,
+            id: "5eddeca87f697b1bcc63037e"
+        },
+        {
+            title: "The night of terror",
+            author: "Alex Fergusson",
+            url: "www.kenwood.com",
+            likes: 7,
+            id: "5edded317f697b1bcc63037f"
+        },
+        {
+            title: "The limelight",
+            author: "Brad Sparrow",
+            url: "www.limelight.com",
+            likes: 14,
+            id: "5ede04762805fb1e809c7fd6"
+        },
+        {
+            title: "The story of mankind",
+            author: "Grasley Mosley",
+            url: "www.mankindstroy.com",
+            likes: 11,
+            id: "5ede90cfe23ee44e90168709"
+        },
+        {
+            title: "The power of love",
+            author: "Anthony Morris",
+            url: "www.anthonymorris.com",
+            likes: 21,
+            id: "5ede92f56a5ada3f8481d788"
+        },
+        {
+            title: "Kings of the jungle",
+            author: "Brad Sparrow",
+            url: "www.junglekings.com",
+            likes: 17,
+            id: "5ede93297e73873520d6e858"
+        },
+        {
+            title: "Love and soccer",
+            author: "Alex Fergusson",
+            url: "www.loveandsoccer.com",
+            likes: 23,
+            id: "5ede939e93c240242c3f9c03"
+        },
+        {
+            title: "The power of Java",
+            author: "Kevin Moore",
+            url: "www.kevinmoore.com",
+            likes: 14,
+            id: "5ede963d7b474c16e877b5ed"
+        },
+        {
+            title: "The adventure of Riina",
+            author: "Alex Fergusson",
+            url: "www.patmason.com",
+            likes: 7,
+            id: "5ede96e17b474c16e877b5ee"
+        }
+    ]
+
+    test('in a blog list with an empty array is none or zero', () => {
+        const result = listHelper.mostBlogs(listWithEmptyContent)
+        expect(result).toBe(0)
+    })
+
+    test('in a blog list with one blog is that author', () => {
+        const result = listHelper.mostBlogs(listWithOneBlog)
+        expect(result).toEqual({
+            author: "Ben Morrisson", 
+            blogs: 1
+        })
+    })
+
+    test('in a blog list with many blogs is the one with highest number of blogs', () => {
+        const result = listHelper.mostBlogs(listWithManyBlogs)
+        expect(result).toEqual({
+            author: "Brad Sparrow", 
+            blogs: 3
         })
     })
 })
--- blogposts/utils/list_helper.js
+++ blogposts/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -16,21 +18,40 @@ const favoriteBlog = (blogs) => {
         return 0
     }
     const likesArray = blogs.map(blog => blog.likes)
-    const copyOfLikesArray = [...likesArray]
-    const indexOfMaxLikes = copyOfLikesArray.indexOf(Math.max(...copyOfLikesArray))
+    const indexOfMaxLikes = likesArray.indexOf(Math.max(...likesArray))
     const blogNeededProperties = {
         title: blogs[indexOfMaxLikes].title,
         author: blogs[indexOfMaxLikes].author,
         likes: blogs[indexOfMaxLikes].likes
     }
-    console.log(blogNeededProperties)
     return blogs.length === 1
         ? { title: blogs.title, author: blogs.author, likes: blogs.likes }
         : blogNeededProperties
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+    const authorsArray = blogs.map(blog => blog.author)
+
+    const authorAndNumberOfBlogs = _.countBy(authorsArray)
+    const authorWithHighestBlogs = _.maxBy(_.keys(authorAndNumberOfBlogs), function (f) {
+
+        return authorAndNumberOfBlogs[f]
+    })
+    console.log(authorAndNumberOfBlogs)
+    console.log('Author with highest blogs', authorWithHighestBlogs)
+    const numberOfBlogs = _.values(authorAndNumberOfBlogs)
+    const highestValue = Math.max(...numberOfBlogs)
+    console.log('Highest Value', highestValue)
+
+    return { author: authorWithHighestBlogs, blogs: highestValue }
 }
 
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
+    mostBlogs,
 }
