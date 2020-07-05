5b7e8465cbd7fad8e81f8ca8e940c52df0494f21
yomites <marshall_400@yahoo.com>
2020-07-05 10:54:50 +0300

    Exercise 4.18. Token-based authentication.
    Exercise 4.18: Bloglost expansion step 6.
    Token-based authentication.

--- blogposts/app.js
+++ blogposts/app.js
@@ -8,6 +8,7 @@ const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -24,6 +25,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
