8e62ba8582129dfaab842d1f36c8740fd5f98b37
yomites <marshall_400@yahoo.com>
2020-07-05 11:34:25 +0300

    Exercise 4.19
    Exercise 4.19: Bloglist expansion step 7. The application has been modified such that adding new blogs is only possiblr if a valid token is sent with the HTTP POST request and the user identified by the token is designated as the creator of the blog.

--- blogposts/controllers/blogs.js
+++ blogposts/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,12 +18,13 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const usersInDb = await User.find({})
-    const userToFind = usersInDb.map(u => u.toJSON())
-    const userId = userToFind[0].id
-
-    const user = await User.findById(userId)
- 
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    
     const blog = new Blog({
         title: body.title,
         author: body.author,
